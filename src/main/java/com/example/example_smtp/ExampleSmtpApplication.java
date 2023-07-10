d5449077294cd53bed96af1b991313ee3e65c3ab
Nguyen Dong Nam <dongnam.japan@gmail.com>
2023-07-10 10:45:00 +0700

    init project

---
+++ src/main/java/com/example/example_smtp/ExampleSmtpApplication.java
@@ -0,0 +1,13 @@
+package com.example.example_smtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSmtpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSmtpApplication.class, args);
+	}
+
+}
